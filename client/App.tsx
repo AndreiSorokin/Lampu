8af5178a47cd53e998c3bc7a6f877a7d6b778109
Andrei Sorokin <72672144+AndreiSorokin@users.noreply.github.com>
2025-04-06 22:09:38 +0300

    added structure

--- client/App.tsx
+++ client/App.tsx
@@ -1,31 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import axios from 'axios';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
 
-export default function App(): JSX.Element {
-  const [events, setEvents] = useState<string[]>([]);
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:3000/events')
-      .then((response) => setEvents(response.data.map((e: any) => e.name)))
-      .catch((error) => console.error(error));
-  }, []);
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Events:</Text>
-      {events.map((event, index) => (
-        <Text key={index}>{event}</Text>
-      ))}
-    </View>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
