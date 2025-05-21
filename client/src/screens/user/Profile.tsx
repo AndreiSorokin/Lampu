b0106bc041de25161b820bca63eb48c023a61c2a
Andrei Sorokin <72672144+AndreiSorokin@users.noreply.github.com>
2025-05-21 12:15:55 +0300

    started profile

--- client/src/screens/user/Profile.tsx
+++ client/src/screens/user/Profile.tsx
@@ -1,11 +1,15 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react'
 import { useTranslation } from 'react-i18next';
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, Pressable } from 'react-native'
+import CustomButton from 'src/components/CustomButton';
+import { RootStackParamList } from 'src/navigation/AppNavigator';
 import { auth } from 'src/utils/firebaseConfig';
 
+type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Organizer'>;
 
-const Profile = () => {
+const Profile = ({ navigation }: { navigation: ProfileScreenNavigationProp }) => {
   const { t } = useTranslation();
   const logOut = async() => {
     try {
@@ -18,12 +22,23 @@ const Profile = () => {
   }
   return (
     <View style={{ flex: 1, padding: 20, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{t('language')}</Text>
       <Text>{t('profile')}</Text>
-      <Text>{t('organazer')}</Text>
-      <Button
-        title='Log out'
+      <View>
+        <Text style={{ margin: 10 }}>Name</Text>
+        <Button
+          title={t('edit')}
+        />
+      </View>
+      <Text>{t('language')}</Text>
+      <CustomButton
+        title={t('organazer')}
+        onPress={() => navigation.navigate('Register')}
+        style={{justifyContent: 'center', alignItems: 'center'}}
+      />
+      <CustomButton
+        title={t('logout')}
         onPress={logOut}
+        style={{justifyContent: 'center', alignItems: 'center'}}
       />
     </View>
   )
